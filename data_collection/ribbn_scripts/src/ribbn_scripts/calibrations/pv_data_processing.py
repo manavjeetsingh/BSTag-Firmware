cc7dab7eeae1d179d6601b55023e40628a6a40d1
manavjeetsingh <manavsingh@cs.stonybrook.edu>
2026-09-15 15:20:41 -0400

    PV data processing

--- data_collection/ribbn_scripts/src/ribbn_scripts/calibrations/pv_data_processing.py
+++ data_collection/ribbn_scripts/src/ribbn_scripts/calibrations/pv_data_processing.py
@@ -1,48 +1,78 @@
 from ribbn_scripts.ref_functions.util_functions import *
 from matplotlib import pyplot as plt
 import numpy as np
-from scipy import signal as sig
+import os
+import json
+import pandas as pd
 
-fn = 'v32-3'
-col_data = read_pickle('C:/git/T2TExperiments/coba/calibrations/PV_data_Dec2025/'+fn+'_pv_dat.pkl')
-# print(col_data)
+save_folder = 'PV_data_Sept2026'
+os.makedirs(save_folder + '/processed', exist_ok=True)
+
+fns = set()
+all_files = os.listdir(save_folder)
+for f in all_files:
+    if f[:17].count('_') != 5:
+        continue
+    fns.add(f[:17])
+
+ENABLE_PLOTTING = False
+
 pwr_range = range(-35, -13, 1)
-plt.figure()
-pv_polynomials = {}
-ctr = 1
-for freq in range(775,1010,10):
-# for freq in [915]:
-# for freq in [775, 915,995]:
-    # plt.title(freq)
-    x = col_data[freq][0:len(pwr_range)]
-    y = np.array(pwr_range)
+target_mVs = [15, 40]
 
-    # print(x)
-    p = np.polyfit(np.log(x), y, 2)
-    p_inv = np.polyfit(y, np.log(x), 2)
-    poly_dict = {'polynomial': p, 'inverse': p_inv}
-    pv_polynomials[freq] = poly_dict
+complete_df = pd.DataFrame(columns=["Tag MAC", "Frequency", "Polynomial", "Inverse"] +
+                            [f"Required Power @{t}mV (dBm)" for t in target_mVs])
 
-    # plt.subplot(7, 4, ctr)
-    plt.plot(x, y, '.')
-    # print(y)
-    # print(x)
-    beautify_graph(True, 'voltage (mV)', 'pwr (dbm)', 'frequency')
-    #
-    xnew = np.linspace(min(x), max(x), 100)
-    target_mV=15
-    print(f"At frequency: {freq} Mhz, {np.polyval(p, np.log(target_mV))} power is required to achieve {target_mV} mV out of ADC.")
-    plt.plot(xnew, np.polyval(p, np.log(xnew)),label=str(freq))
-    plt.legend()
-    ctr = ctr + 1
+for fn in fns:
+    col_data = read_pickle(save_folder+'/'+fn+'_pv_dat.pkl')
 
-# beautify_graph(True, 'voltage (mV)', 'pwr (dbm)', 'frequency')
-# plt.savefig('../../plots/calibration_images/pv/rx.pdf')
-plt.xlabel('mV')
-plt.ylabel('dbm')
-plt.show()
+    if ENABLE_PLOTTING:
+        plt.figure()
+    pv_polynomials = {}
+    rows = []
+    for freq in range(700, 1010, 10):
+        x = col_data[freq][0:len(pwr_range)]
+        y = np.array(pwr_range)
 
-write_pickle('C:/git/T2TExperiments/coba/calibrations/PV_data_Dec2025/'+fn+'_pv_polynomials_rx.pkl', pv_polynomials)
+        p = np.polyfit(np.log(x), y, 2)
+        p_inv = np.polyfit(y, np.log(x), 2)
+        pv_polynomials[freq] = {'polynomial': p.tolist(), 'inverse': p_inv.tolist()}
+
+        xnew = np.linspace(min(x), max(x), 100)
+        req_pwrs = {}
+        for target_mV in target_mVs:
+            req_pwr = float(np.polyval(p, np.log(target_mV)))
+            req_pwrs[f"Required Power @{target_mV}mV (dBm)"] = req_pwr
+            print(f"[{fn}] At frequency: {freq} Mhz, {req_pwr} power is required to achieve {target_mV} mV out of ADC.")
+
+        rows.append({
+            "Tag MAC": fn,
+            "Frequency": freq,
+            "Polynomial": p.tolist(),
+            "Inverse": p_inv.tolist(),
+            **req_pwrs,
+        })
+
+        if ENABLE_PLOTTING:
+            plt.plot(x, y, '.')
+            beautify_graph(True, 'voltage (mV)', 'pwr (dbm)', 'frequency')
+            plt.plot(xnew, np.polyval(p, np.log(xnew)), label=str(freq))
+
+    if ENABLE_PLOTTING:
+        plt.title(fn)
+        plt.xlabel('mV')
+        plt.ylabel('dbm')
+        plt.legend()
+
+    complete_df = pd.concat([complete_df, pd.DataFrame(rows)], ignore_index=True)
+
+    with open(f"{save_folder}/processed/{fn}_pv_polynomials.json", 'w') as j_f:
+        json.dump(pv_polynomials, j_f, indent=4)
+
+complete_df.to_csv(f"{save_folder}/processed/all_pv_polynomials.csv", index=False)
+
+if ENABLE_PLOTTING:
+    plt.show()
 
 
 # freq=915
